8f33922b9e54d5db2eac4e65cb83ddf09ede7cdf
sun-way <37132870+sun-way@users.noreply.github.com>
2018-07-16 14:23:46 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,15 +1,15 @@
-<?php
+<?php 
 require_once('./config.php');
 require_once('./getFileList.php');
 if (isset($_FILES['testfile']) && !empty($_FILES['testfile'])) {
     $fileType = $_FILES['testfile']['type'];
     $tmpPath = $_FILES['testfile']['tmp_name'];
     if ($fileType !== 'application/json') {
-        exit('File not instance of json. Please upload another file.');
+      exit('File not instance of json. Please upload another file.');
     }
     $id = getFileList(['targetDir' => $testsDir, 'returnCount' => true]);
     $fileName = 'id' . ++$id . '.json';
-
+    
     move_uploaded_file($tmpPath, $testsDir . $fileName);
     echo 'File did upload';
 }
@@ -19,32 +19,32 @@ if (isset($_FILES['testfile']) && !empty($_FILES['testfile'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Tests uploader</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Tests uploader</title>
 </head>
 <body>
 
-<form action="./admin.php" method="POST" enctype="multipart/form-data">
+  <form action="./admin.php" method="POST" enctype="multipart/form-data">
     <label>
-        <div>Выберите файл:</div>
-        <div><input name="testfile" type="file" accept="application/json"></div>
+      <div>Выберите файл:</div>
+      <div><input name="testfile" type="file" accept="application/json"></div>
     </label>
 
     <div>
-        <button type="submit">
-            Отправить
-        </button>
+      <button type="submit">
+        Отправить
+      </button>
     </div>
-</form>
+  </form>
 
 
-<style>
+  <style>
     div {
-        padding: 10px 0;
+      padding: 10px 0;
     }
-</style>
-
+  </style>
+  
 </body>
 </html>
