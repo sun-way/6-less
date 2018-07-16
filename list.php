ef0fdffcdb628cd3b28c5e0cb31dca49f0c3a7da
sun-way <37132870+sun-way@users.noreply.github.com>
2018-07-16 14:27:28 +0300

    Update list.php

--- list.php
+++ list.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 require_once('./config.php');
 require_once('./getFileList.php');
 $list = getFileList(['targetDir' => $testsDir]);
@@ -6,20 +6,20 @@ $list = getFileList(['targetDir' => $testsDir]);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Test list</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Test list</title>
 </head>
 <body>
 
-<h1>Список тестов:</h1>
-
-<ul>
+  <h1>Список тестов:</h1>
+  
+  <ul>
     <?php foreach($list as $filename): ?>
-        <li><?php echo $filename ?></li>
+      <li><?php echo $filename ?></li>
     <? endforeach ?>
-</ul>
+  </ul>
 
 </body>
 </html>
